a17376bf58c59d213860d8af39ebd56f21a3179b
Nadav <nadav.matalon@gmail.com>
2014-11-12 15:34:47 +0000

    code refactoring of js functions

--- public/javascripts/battleships_web.js
+++ public/javascripts/battleships_web.js
@@ -1,22 +1,19 @@
 function togglestyle(element) {
-	if(element.className == "coord") {
-		element.className="selected";
-		coords.value+=element.value+', ';
-		// console.log(coords.value);
-		// console.log(element.className);
+	if(element.className === "coord") {
+		element.className = "selected";
+		coords.value += element.value + ', ';
 	} else {
 		element.className="coord";
 		var new_coords = coords.value.split(', ');
 		remove(new_coords, element.value);
 		coords.value = new_coords.join(', ');
-		// console.log(coords.value);
-		// console.log(element.className);
 	}
 }
+
 function remove(array, item) {
-		for(var i = array.length; i--;) {
-  		if(array[i] === item) {
-      		array.splice(i, 1);
-  		}
+	for(var i = array.length; i--;) {
+		if(array[i] === item) {
+			array.splice(i, 1);
+		}
 	}
 }
